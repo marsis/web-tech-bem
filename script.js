e9f81992b86d3ed0ffc2d012bc8a2a0b5f8a415d
marsis <logozynski@gmail.com>
2016-08-23 16:24:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+var val = document.getElementById("myRange").value =value;
+var monthCoefficient = data.multiplier;
+var rezult;
+elem.onclick = function(){
+
+    return rezult = val * monthCoefficient;
+    
+}
+myRange.onclick
+function renderAccounts(data){
+
+
+})
+var data = [
+    {account: '11970', name: 'Galax', multiplier: 1.265, perYear: 2.7},
+    {account: '7348', name: 'Veronica', multiplier: 1.12, perYear: 2.0},
+    {account: '10555', name: 'Dragonfly', multiplier: 1.023, perYear: 1.796},
+    {account: '10504', name: 'Condor', multiplier: 1.04, perYear: 1.81},
+    {account: '10469', name: 'Shark_007', multiplier: 1.17, perYear: 2.13},
+]
+
+renderAccounts(data);
